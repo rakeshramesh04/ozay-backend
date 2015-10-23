114e718300eddb376a7a7a86d49f4248520500ce
ozayllc <svarma@HERRING-WS16.herringcreek.corp>
2015-10-23 19:14:10 -0400

    emailCount

--- src/main/java/com/ozay/service/MailService.java
+++ src/main/java/com/ozay/service/MailService.java
@@ -71,7 +71,7 @@ public class MailService {
     }
 
     public int sendGrid(Notification notification, List<NotificationRecord> notificationRecords, String baseUrl){
-        SendGrid sendgrid = new SendGrid("OzayOrg", "SendGrid");
+        SendGrid sendgrid = new SendGrid("OzayOrg", "Ozaysyzn1124");
 
         int sentCount = 0;
 
